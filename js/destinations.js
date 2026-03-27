28883bea6dd24485fff5c8623acb7d1220857b1f
Macario-ECE <macariovinothfernandez@gmail.com>
2026-03-27 20:47:59 +0530

    Add files via upload

---
+++ js/destinations.js
@@ -0,0 +1,17 @@
+const container = document.getElementById("destinationsContainer");
+
+destinations.forEach(dest => {
+
+    const card = `
+  <div class="card">
+        <img src="${dest.image}">
+        <div class="card-content">
+            <h3>${dest.title}</h3>
+            <p>${dest.description}</p>
+        </div>
+        <small>Explore now →</small>
+    </div>
+    `;
+
+    container.innerHTML += card;
+});
