a6de3ac7972a1c1a69dcf1e9ef5b03d8117101fa
Macario-ECE <macariovinothfernandez@gmail.com>
2026-03-27 16:38:01 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -22,7 +22,31 @@ const packages = [
     price: "₹9,999",
     duration: "4 Days",
     image: "images/kerala.jpg"
-  }
+  },
+{
+        image: "images/dubai.jpg",
+        title: "Dubai Tour",
+        price: "₹40,000",
+        duration: "6 Days"
+    },
+    {
+        image: "images/paris.jpg",
+        title: "Paris Trip",
+        price: "₹60,000",
+        duration: "7 Days"
+    },
+    {
+        image: "images/bali.jpg",
+        title: "Bali Vacation",
+        price: "₹35,000",
+        duration: "5 Days"
+    },
+    {
+        image: "images/singapore.jpg",
+        title: "Singapore Tour",
+        price: "₹45,000",
+        duration: "4 Days"
+    }
 ];
 
 const reviews = [
