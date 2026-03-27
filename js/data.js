be9f771b705676007cdebdf6ff77efe09447bec4
Macario-ECE <macariovinothfernandez@gmail.com>
2026-03-27 06:57:30 +0530

    Create data.js
    In this file what u created in html hardcoded is changed here in js

---
+++ js/data.js
@@ -0,0 +1,33 @@
+const packages = [
+  {
+    title: "Goa Trip",
+    price: "₹8,999",
+    duration: "3 Days",
+    image: "images/goa.jpg"
+  },
+  {
+    title: "Manali Tour",
+    price: "₹10,999",
+    duration: "4 Days",
+    image: "images/manali.jpg"
+  },
+  {
+    title: "Kashmir Tour",
+    price: "₹12,999",
+    duration: "5 Days",
+    image: "images/kashmir.jpg"
+  },
+  {
+    title: "Kerala Tour",
+    price: "₹9,999",
+    duration: "4 Days",
+    image: "images/kerala.jpg"
+  }
+];
+
+const reviews = [
+  "Amazing experience!",
+  "Best travel service ever!",
+  "Highly recommended!",
+  "Loved every moment!"
+];
