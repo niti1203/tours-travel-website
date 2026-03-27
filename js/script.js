ea8cedb11b86cd536a922433624ef6a4dafb4dac
Nitika <goniti12@gmail.com>
2026-03-27 18:49:08 +0530

    Add booking page with form validation

--- js/script.js
+++ js/script.js
@@ -48,3 +48,72 @@ document
     alert("Message sent successfully!");
 
 });
+
+
+
+document
+.getElementById("bookingForm")
+.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    let name =
+    document.getElementById("bname").value;
+
+    let email =
+    document.getElementById("bemail").value;
+
+    let destination =
+    document.getElementById("destination").value;
+
+    let date =
+    document.getElementById("date").value;
+
+    let people =
+    document.getElementById("people").value;
+
+    if (name === "") {
+
+        alert("Please enter your name");
+        return;
+
+    }
+
+    if (email === "") {
+
+        alert("Please enter your email");
+        return;
+
+    }
+
+    if (!email.includes("@")) {
+
+        alert("Enter a valid email");
+        return;
+
+    }
+
+    if (destination === "") {
+
+        alert("Please select a destination");
+        return;
+
+    }
+
+    if (date === "") {
+
+        alert("Please select a travel date");
+        return;
+
+    }
+
+    if (people === "" || people <= 0) {
+
+        alert("Enter valid number of people");
+        return;
+
+    }
+
+    alert("Booking successful!");
+
+});
